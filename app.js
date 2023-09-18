164b5429462e57d4ea7968a0b418ea5c19a5912d
Mir Billadin <mir.billadin4@gmail.com>
2023-09-18 22:22:11 +0600

    getAllTask route setup

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
 
+//Middleware
+app.use(express.json());
 
 //Routes
 app.get('/hello', (req, res) => {
     res.send('Task manager app');
 })
 
+
+app.use('/api/v1/tasks/', tasks);
 
 
 const port = 5000;
