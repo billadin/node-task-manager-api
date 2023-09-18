1fe0bee949348cdf3116d60f9b062abddf3e46d5
Mir Billadin <mir.billadin4@gmail.com>
2023-09-18 23:13:19 +0600

    db connection setup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config()
 
 //Middleware
 app.use(express.json());
@@ -16,4 +18,15 @@ app.use('/api/v1/tasks/', tasks);
 
 const port = 5000;
 
-app.listen(port, console.log("Server is listening"));
+const start = async ()=> {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log("Server is listening"));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+start()
+
